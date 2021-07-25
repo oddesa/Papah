7b1ae34089afa66c2d261b9d00a0d2da8b2a8ba5
Kane <jehnsen.hirena@sci.ui.ac.id>
2021-07-25 23:45:56 +0700

    "adding some structure folders and file"

---
+++ Papah/Presentation/Tantangan/Tantangan Detail/TantanganDetailController.swift
@@ -0,0 +1,31 @@
+//
+//  TantanganDetailController.swift
+//  Papah
+//
+//  Created by Jehnsen Hirena Kane on 25/07/21.
+//
+
+import UIKit
+
+class TantanganDetailController: UIViewController {
+    
+    private let viewModel = TantanganDetailVielModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Papah/Presentation/Tantangan/Tantangan Detail/TantanganDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TantanganDetailVielModel.swift
+//  Papah
+//
+//  Created by Jehnsen Hirena Kane on 25/07/21.
+//
+
+import UIKit
+
+class TantanganDetailViewModel: NSObject {
+
+}
