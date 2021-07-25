e57990f637bde9187ce698a9ce7a29b535657e56
Kane <jehnsen.hirena@sci.ui.ac.id>
2021-07-25 22:48:00 +0700

    adding gitignore and attributes

---
+++ Papah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Papah
+//
+//  Created by Jehnsen Hirena Kane on 25/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
