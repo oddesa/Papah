67fbbc7de5bf86cb3f8dd8539f902444eaa108b7
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-07-26 00:17:18 +0700

    update

---
+++ Papah/Core/CoreDataManager.swift
@@ -0,0 +1,79 @@
+//
+//  CoreDataManage.swift
+//  Papah
+//
+//  Created by Jehnsen Hirena Kane on 25/07/21.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    static let sharedManager = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: Constants.dataModel)
+        
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.shouldDeleteInaccessibleFaults = true
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        
+        return container
+    }()
+    
+    func saveContext () {
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    func preloadData(){
+     
+    }
+    
+    func deleteAllData(){
+        
+        let storeCoordinator = persistentContainer.persistentStoreCoordinator
+        let storeDescription = persistentContainer.persistentStoreDescriptions[0]
+        guard let storeURL = storeDescription.url else {
+            return
+        }
+        
+        do {
+            try storeCoordinator.destroyPersistentStore(at: storeURL, ofType: NSSQLiteStoreType, options: nil)
+        }
+        catch {
+            return
+        }
+        
+        storeCoordinator.addPersistentStore(with: storeDescription) {
+            (persistentStoreCoordinator, error) in
+            
+            if let error = error {
+                print("\(error)")
+            }
+        }
+        
+        //Reload the data again
+        preloadData()
+    }
+    
+}
+
---
+++ Papah/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Papah
+//
+//  Created by Jehnsen Hirena Kane on 25/07/21.
+//
+
+import Foundation
+import UIKit
+
+class Constants  {
+    
+    static let APP_VERSION = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    static let dataModel = "Papah"
+    
+}
