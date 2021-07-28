7f0b78fa47c19a59ca6f54261d5d80290a4e4b38
dearjeah <j.delvina@gmail.com>
2021-07-28 13:06:12 +0700

    update static db + layout

--- Papah/Presentation/Eksplor/Eksplor List/Filter/EksplorListFilterViewModel.swift
+++ Papah/Presentation/Eksplor/Eksplor List/Filter/EksplorListFilterViewModel.swift
@@ -7,6 +7,32 @@
 
 import UIKit
 
+struct FilterWaste {
+    let id: String = UUID().uuidString
+    let img: UIImage?
+    let title: String?
+    var isSelected: Bool?
+    
+    static func getFilterWasteData() -> [FilterWaste]{
+        var fwData = [FilterWaste]()
+        //let obImage = BagelsOnboardingImage()
+        
+        let fwData1 = FilterWaste(img: #imageLiteral(resourceName: "WhatsApp Image 2021-07-19 at 08.50.13"), title: "Elektronik", isSelected: false)
+        let fwData2 = FilterWaste(img: #imageLiteral(resourceName: "WhatsApp Image 2021-07-19 at 08.50.13"), title: "Kardus", isSelected: false)
+        let fwData3 = FilterWaste(img: #imageLiteral(resourceName: "WhatsApp Image 2021-07-19 at 08.50.13"), title: "Kertas", isSelected: false)
+        let fwData4 = FilterWaste(img: #imageLiteral(resourceName: "WhatsApp Image 2021-07-19 at 08.50.13"), title: "Perabot", isSelected: false)
+        let fwData5 = FilterWaste(img: #imageLiteral(resourceName: "WhatsApp Image 2021-07-19 at 08.50.13"), title: "Plastik", isSelected: false)
+        
+        fwData.append(fwData1)
+        fwData.append(fwData2)
+        fwData.append(fwData3)
+        fwData.append(fwData4)
+        fwData.append(fwData5)
+
+        return fwData
+    }
+}
+
 class EksplorListFilterViewModel: NSObject {
 
 }
