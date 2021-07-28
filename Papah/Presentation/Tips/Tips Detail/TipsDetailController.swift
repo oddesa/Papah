47ee06735ee15a9747a9d2c724aa13fd8e1c1a84
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-07-28 13:54:48 +0700

    First iteration for TipsDetail

--- Papah/Presentation/Tips/Tips Detail/TipsDetailController.swift
+++ Papah/Presentation/Tips/Tips Detail/TipsDetailController.swift
@@ -7,25 +7,34 @@
 
 import UIKit
 
-class TipsDetailController: UIViewController {
+class TipsDetailController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tipsDetailDummy.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = tipsDetailDummy[indexPath.row]
+        let cell = tipsDetailCollection.dequeueReusableCell(withReuseIdentifier: "tipsDetailCell", for: indexPath) as! TipsDetailCollectionCell
+//        cell.item = surah
+        cell.setTipsDetailByCategory(with: item)
+        print("CELL \(cell)")
+        return cell
+    }
+    
+    private let tipsDetailDummy = [ TipsDetail(image: "yes", title: "Memilah Sampah Plastik", desc: "plastik lama terurai"), TipsDetail(image: "yes", title: "Menyalurkan Sampah Plastik", desc: "plastik lama terurai"), TipsDetail(image: "yes", title: "Merecycle plastic", desc: "plastik lama terurai")]
     
     private let viewModel = TipsDetailViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tipsDetailCollection.delegate = self
+        tipsDetailCollection.dataSource = self
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBOutlet weak var tipsDetailCollection: UICollectionView!
+}
+struct TipsDetail{
+    let image: String
+    let title: String
+    let desc: String
 }
