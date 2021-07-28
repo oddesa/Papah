5ad3dfdf958aee851beb8515a634723236d0c56a
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-07-28 12:29:18 +0700

    add tantangan list page

--- Papah/Presentation/Tantangan/Tantangan List/TantanganListController.swift
+++ Papah/Presentation/Tantangan/Tantangan List/TantanganListController.swift
@@ -9,22 +9,166 @@ import UIKit
 
 class TantanganListController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let sectionLevel = 0
+    private let sectionMonthly = 1
+    private let sectionRewards = 2
+
+    static let footerHeight = 100
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
 
-        // Do any additional setup after loading the view.
+        registerNib()
+        
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TantanganListController: UITableViewDelegate, UITableViewDataSource {
+    
+    func registerNib() {
+        tableView.register(UINib(nibName: TantanganLevelTableCell.cellIdentifier(), bundle: nil), forCellReuseIdentifier: TantanganLevelTableCell.cellIdentifier())
+        tableView.register(UINib(nibName: TantanganEarningCell.cellIdentifier(), bundle: nil), forCellReuseIdentifier: TantanganEarningCell.cellIdentifier())
+        tableView.register(UINib(nibName: TantanganMonthlyCell.cellIdentifier(), bundle: nil), forCellReuseIdentifier: TantanganMonthlyCell.cellIdentifier())
+        tableView.register(UINib(nibName: TantanganRewardTablecell.cellIdentifier(), bundle: nil), forCellReuseIdentifier: TantanganRewardTablecell.cellIdentifier())
+        tableView.register(UINib(nibName: "TantanganMonthlyHeadCell", bundle: nil), forHeaderFooterViewReuseIdentifier: "TantanganMonthlyHeadCell")
+        tableView.register(UINib(nibName: "TantanganRewardHeadCell", bundle: nil), forHeaderFooterViewReuseIdentifier: "TantanganRewardHeadCell")
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return [sectionLevel, sectionMonthly, sectionRewards].count
+    }
+    
+    func tableviewIdentifier(section: Int) -> [String] {
+        var identifiers = [String]()
+        
+        if section == sectionLevel {
+            identifiers.append(TantanganLevelTableCell.cellIdentifier())
+            identifiers.append(TantanganEarningCell.cellIdentifier())
+        }
+        if section == sectionMonthly {
+            identifiers.append(TantanganMonthlyCell.cellIdentifier())
+        }
+        if section == sectionRewards {
+            identifiers.append(TantanganRewardTablecell.cellIdentifier())
+        }
+        
+        return identifiers
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.section == sectionRewards {
+            return 190
+        }
+        
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == sectionMonthly {
+            return 45
+        }
+        if section == sectionRewards {
+            return 45
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if section == sectionMonthly {
+            
+            guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "TantanganMonthlyHeadCell") as? TantanganMonthlyHeadCell else {
+                return UIView()
+            }
+            let backgroundView = UIView(frame: headerView.bounds)
+            backgroundView.backgroundColor = .clear
+            headerView.backgroundView = backgroundView
 
+            return headerView
+        }
+        
+        if section == sectionRewards {
+            
+            guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "TantanganRewardHeadCell") as? TantanganRewardHeadCell else {
+                return UIView()
+            }
+            let backgroundView = UIView(frame: headerView.bounds)
+            backgroundView.backgroundColor = .clear
+            headerView.backgroundView = backgroundView
+
+            return headerView
+        }
+        
+        return UIView()
+     
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+//        return CGFloat(TantanganListController.footerHeight)
+//    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableviewIdentifier(section: section).count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let identifier = self.tableviewIdentifier(section: indexPath.section)[indexPath.row]
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) else {
+            return UITableViewCell()
+        }
+        
+        switch identifier {
+        case TantanganLevelTableCell.cellIdentifier():
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TantanganLevelTableCell.cellIdentifier()) as? TantanganLevelTableCell else {
+                return UITableViewCell()
+            }
+            cell.selectionStyle = .none
+            
+            return cell
+        case TantanganEarningCell.cellIdentifier():
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TantanganEarningCell.cellIdentifier()) as? TantanganEarningCell else {
+                return UITableViewCell()
+            }
+          
+            cell.selectionStyle = .none
+            
+            return cell
+        case TantanganMonthlyCell.cellIdentifier():
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TantanganMonthlyCell.cellIdentifier()) as? TantanganMonthlyCell else {
+                return UITableViewCell()
+            }
+          
+            cell.selectionStyle = .none
+            
+            return cell
+        case TantanganRewardTablecell.cellIdentifier():
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TantanganRewardTablecell.cellIdentifier()) as? TantanganRewardTablecell else {
+                return UITableViewCell()
+            }
+            
+            cell.setData()
+          
+            cell.selectionStyle = .none
+            
+            return cell
+     
+        default:
+            cell.selectionStyle = .none
+            return cell
+        }
+    }
+    
+    
 }
