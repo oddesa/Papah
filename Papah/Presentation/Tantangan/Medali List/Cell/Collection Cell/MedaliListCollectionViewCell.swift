ffc8f8dab04b71513318671c50dffdd731f37b42
Rizqi Ahmad Kurniawan <rizqikurniawan233@gmail.com>
2021-07-28 22:44:42 +0700

    Penambahan fitur "MedaliDetails" dan "MedaliList"

---
+++ Papah/Presentation/Tantangan/Medali List/Cell/Collection Cell/MedaliListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MedaliListCollectionViewCell.swift
+//  Papah
+//
+//  Created by Rizqi Ahmad Kurniawan on 28/07/21.
+//
+
+import UIKit
+
+class MedaliListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
