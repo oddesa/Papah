452dfc8d7e195e40d75e10e775a459739e619419
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-07-30 09:33:52 +0700

    add tips coredata

--- Papah/Core/Repositories/TipsDataRepository.swift
+++ Papah/Core/Repositories/TipsDataRepository.swift
@@ -12,134 +12,149 @@ import UIKit
 class TipsDataRepository {
     
     static let shared = TipsDataRepository()
-    let entityName = "Sampah"
+    let entityName = Sampah.self.description()
     
-//    func insertEvaluations(completed: Bool,
-//                           level: Int,
-//                           desc: String,
-//                           editedImage: UIImage,
-//                           rawImage: UIImage, challenge: Challenges) {
-//        
-//        do {
-//            let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-//            
-//            //Add evaluation photo
-//            let evaluationDetails = EvaluationDetails(context: context)
-//            evaluationDetails.completed = false
-//            evaluationDetails.desc = desc
-//            evaluationDetails.edited_image = editedImage.jpegData(compressionQuality: 1.0)
-//            evaluationDetails.raw_image = rawImage.jpegData(compressionQuality: 1.0)
-//            evaluationDetails.challenge = challenge
-//
-//            
-//            //Add evaluation data
-//            
-//            if getEvaluationByLevel(level: level) == nil {
-//                
-//                print("CALLED BY WHOM??")
-//                
-//                let entity: Evaluations = .init(context: context)
-//                entity.level = Int32(level)
-//                entity.addToEvaluationDetail(evaluationDetails)
-//            } else {
-//                if let entity = getEvaluationByLevel(level: level) {
-//                    entity.addToEvaluationDetail(evaluationDetails)
-//                }
-//            }
-//            
-//            try context.save()
-//            
-//        } catch {
-//            
-//        }
-//
-//    }
-//    
-//    func getEvaluationByLevel(level: Int) -> Evaluations? {
-//        
-//        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
-//        fetchRequest.predicate = NSPredicate(format: "level == %d", level)
-//        
-//        do {
-//            
-//            let item = try context.fetch(fetchRequest) as! [Evaluations]
-//            
-//            return item.first
-//        } catch let error as NSError {
-//            print("Could not save. \(error), \(error.userInfo)")
-//        }
-//        
-//        return nil
-//    }
-//    
-//    
-//    func getAllEvaluations() -> [Evaluations] {
-//        
-//        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
-//        
-//        do {
-//            
-//            let item = try context.fetch(fetchRequest) as! [Evaluations]
-//            
-//            return item
-//        } catch let error as NSError {
-//            print("Could not save. \(error), \(error.userInfo)")
-//        }
-//        
-//        return []
-//    }
-//
-//    func updateEvaluationDetail(completed:Bool, desc: String, editedImage: UIImage, data: EvaluationDetails){
-//        
-//        do {
-//            
-//            let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-//
-//            let entityData = data
-//
-//            entityData.completed = completed
-//            data.desc = desc
-//            data.edited_image = editedImage.jpegData(compressionQuality: 1.0)
-//            
-//            try context.save()
-//
-//        } catch {
-//
-//        }
-//    }
-//
-//    
-//    
-//    func deleteEvaluations(data: Evaluations){
-//
-//        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-//
-//        do {
-//            context.delete(data)
-//            try context.save()
-//
-//        } catch let error as NSError {
-//            print("Could not save. \(error), \(error.userInfo)")
-//        }
-//
-//    }
-//    
-//    
-//    func deleteAllEvaluations(){
-//
-//        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
-//
-//        do {
-//            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: entityName)
-//            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-//
-//            try context.execute(deleteRequest)
-//        } catch let error as NSError {
-//            print("Could not save. \(error), \(error.userInfo)")
-//        }
-//
-//    }
-
+    func insertTips(title: String,
+                    desc: String,
+                    sampahId: Int,
+                    image: UIImage) {
+        
+        do {
+            let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+            
+            //Add evaluation photo
+            let evaluationDetails = Sampah(context: context)
+            evaluationDetails.title = title
+            evaluationDetails.desc = desc
+            evaluationDetails.image = image.jpegData(compressionQuality: 1.0)
+            evaluationDetails.sampah_id = Int32(sampahId)
+            
+            try context.save()
+            
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+    
+    func insertTipsDetail(title: String,
+                          detail: String,
+                          sampahId: Int,
+                          sampahDetailId: Int,
+                          image: UIImage) {
+        
+        do {
+            
+            let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+            
+            if let tips = getTipsById(sampahId: sampahId) {
+                
+                let tipsDetail = SampahDetail(context: context)
+                tipsDetail.title = title
+                tipsDetail.detail = detail
+                tipsDetail.image = image.jpegData(compressionQuality: 1.0)
+                tipsDetail.sampah_id = Int32(sampahId)
+                
+                tips.addToSampahDetail(tipsDetail)
+                
+                try context.save()
+            }
+            
+            
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+    func getTipsById(sampahId: Int) -> Sampah? {
+        
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: "sampah_id == %d", sampahId)
+        
+        do {
+            
+            let item = try context.fetch(fetchRequest) as? [Sampah]
+            
+            return item?.first
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+        
+        return nil
+    }
+    
+    
+    
+    func getAllTips() -> [Sampah]? {
+        
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        
+        do {
+            
+            let item = try context.fetch(fetchRequest) as? [Sampah]
+            
+            return item
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+        return []
+    }
+    
+    
+    func getTipsDetailById(sampahId: Int) -> [SampahDetail]? {
+        
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: "sampah_id == %d", sampahId)
+        
+        do {
+            
+            let item = try context.fetch(fetchRequest) as? [SampahDetail]
+            
+            return item
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+        return nil
+    }
+    
+    
+    
+    func deleteTips(data: Sampah) {
+        
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        
+        do {
+            context.delete(data)
+            try context.save()
+            
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+    
+    func deleteAllTips() {
+        
+        let context = CoreDataManager.sharedManager.persistentContainer.viewContext
+        
+        do {
+            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: entityName)
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            
+            try context.execute(deleteRequest)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
 }
---
+++ PapahTests/TipsRepositoryUnitTest.swift
@@ -0,0 +1,68 @@
+//
+//  PapahTests.swift
+//  PapahTests
+//
+//  Created by Jehnsen Hirena Kane on 25/07/21.
+//  swiftlint:disable all
+
+import XCTest
+import CoreData
+
+@testable import Papah
+
+class TipsRepositoryUnitTest: XCTestCase {
+    var tipsRepo = TipsDataRepository.shared
+    let entityName = Sampah.self.description()
+
+    override func tearDown() {
+        super.tearDown()
+        tipsRepo.deleteAllTips()
+    }
+    func test_tipsInsertData() {
+        
+        let expectation = self.expectation(description: "Should return correct data")
+        expectation.expectedFulfillmentCount = 1
+        
+        // when
+        tipsRepo.insertTips(title: "test", desc: "test", sampahId: 1, image: UIImage.init())
+        
+        let data = tipsRepo.getAllTips()
+        
+        if let result = data?.first {
+            XCTAssertEqual(result.title, "test")
+            expectation.fulfill()
+        } else {
+            XCTFail("Should return proper response")
+        }
+        
+        // then
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func test_tipsDetailInsertData() {
+        
+        let expectation = self.expectation(description: "Should return correct data")
+        expectation.expectedFulfillmentCount = 1
+        
+        // when
+        
+//        let entity = NSEntityDescription.entity(forEntityName: entityName, in: CoreDataManager.sharedManager.persistentContainer.viewContext)
+//        let challenges = SampahDetail(entity: entity!, insertInto: CoreDataManager.sharedManager.persistentContainer.viewContext)
+
+        tipsRepo.insertTips(title: "test1", desc: "test", sampahId: 1, image: UIImage.init())
+        tipsRepo.insertTipsDetail(title: "test2", detail: "test", sampahId: 1, sampahDetailId: 1, image: UIImage.init())
+        
+        let data = tipsRepo.getAllTips()
+        
+        if let result = data?.first, let tipsDetail = result.sampahDetail?.allObjects.last as? SampahDetail {
+            XCTAssertEqual(result.title, "test1")
+            XCTAssertEqual(tipsDetail.title, "test2")
+            expectation.fulfill()
+        } else {
+            XCTFail("Should return proper response")
+        }
+        
+        // then
+        wait(for: [expectation], timeout: 5)
+    }
+}
