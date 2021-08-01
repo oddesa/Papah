ceef4b323d8ba78af9d8dffbc6ba7c937b14ebc5
Kane <jehnsen.hirena@sci.ui.ac.id>
2021-08-01 22:35:01 +0700

    fix uisearch controller behaviour + add logic to filter and sort data

--- Papah/Presentation/Eksplor/Eksplor List/EksplorListController.swift
+++ Papah/Presentation/Eksplor/Eksplor List/EksplorListController.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
-class EksplorListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class DummyDataUhuy {
+    let nama: String
+    let categori: String
+    init(nama: String, categori: String) {
+        self.nama = nama
+        self.categori = categori
+    }
+}
+
+
+class EksplorListController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
     
     let strings = ["asdfefsa", "hahahah", "xoxoxoox"]
     var searchBarCont = UISearchController()
@@ -19,38 +29,93 @@ class EksplorListController: UIViewController, UITableViewDataSource, UITableVie
     
     private var viewModel: EksplorListViewModel?
     
-    init(viewModel: EksplorListViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    
-    
+//    init(viewModel: EksplorListViewModel) {
+//        self.viewModel = viewModel
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.searchController = searchBarCont
         
+        searchBarCont.searchResultsUpdater = self
+        searchBarCont.obscuresBackgroundDuringPresentation = false
+        searchBarCont.searchBar.setValue("Batalkan", forKey: "cancelButtonText")
+        
+        setupNib()
+    }
+    
+    // MARK: - UISearchController
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        let splited = text.components(separatedBy: " ")
+        
+        let aaa = DummyDataUhuy(nama: "Ayam Pedes", categori: "Goreng")
+        let a11 = DummyDataUhuy(nama: "Ayam Asin Rebus", categori: "Rebus")
+        let bbb = DummyDataUhuy(nama: "Ayam Asin", categori: "Rebus")
+        let ccc = DummyDataUhuy(nama: "Ayam Pahit", categori: "Bakar")
+        let ddd = DummyDataUhuy(nama: "Ikan Duyung", categori: "Rebus")
+        
+        var dummyUntukDifilter = [aaa, a11, bbb, ccc, ddd]
+        var filteredData: [String] = []
+        
+        
+        for dummy in dummyUntukDifilter {
+            for word in splited {
+                if dummy.nama.lowercased().contains(word.lowercased()) || dummy.categori.lowercased().contains(word.lowercased()){
+                    filteredData.append(dummy.nama)
+                    guard let idx = dummyUntukDifilter.firstIndex(where: { $0 === dummy }) else {return}
+                    dummyUntukDifilter.remove(at: idx)
+                }
+            }
+        }
+        
+        //logic sorting
+        filteredData = filteredData.sorted()
+//        var users = [
+//            User(firstName: "Jemima", home: "Alabama"),
+//            User(firstName: "Peter", home: "Bogor"),
+//            User(firstName: "David", home: "AAA"),
+//            User(firstName: "Kelly", home: "zebratown"),
+//            User(firstName: "Isabella", home: "lololo")
+//        ]
+//
+//        users.sort {
+//            $0.firstName > $1.firstName
+//        }
+//
+//        let sortedUsers = users.sorted {
+//            $0.firstName < $1.firstName
+//        }
+//
+//        users.sort {
+//            $0.home < $1.home
+//        }
+
+        
+        
+        print("ini filtered data \(filteredData)")
+        print("ini dummy \(dummyUntukDifilter)")
+    }
+    
+    
+    // MARK: - TableView DataSource
+    
+    func setupNib() {
         let nibTable = UINib(nibName: "ExplorListTableCell", bundle: nil)
         tableViewOutlet.register(nibTable, forCellReuseIdentifier: "ExplorListTableCell")
         let nibTable1 = UINib(nibName: "EksplorListFilterCollectionTableCell", bundle: nil)
         tableViewOutlet.register(nibTable1, forCellReuseIdentifier: "EksplorListFilterCollectionTableCell")
-        
-        
-        filteredData = strings
-        filterStrings = ["asdas", "asdasda"] // "asdasd"]
-        
-        navigationItem.searchController = searchBarCont
-        self.searchBarCont.searchBar.setValue("Batalkan", forKey: "cancelButtonText")
     }
     
-    // MARK: - TableView DataSource
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -106,104 +171,4 @@ class EksplorListController: UIViewController, UITableViewDataSource, UITableVie
 }
 
 
-//extension EksplorListController: UICollectionViewDelegate, UICollectionViewDataSource {
-//    func setupCollectionView() {
-//        collectionView?.showsHorizontalScrollIndicator = false
-//
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
-//
-//        collectionView?.register(UINib(nibName: MedicineTableCellItem.identifier, bundle: nil), forCellWithReuseIdentifier: MedicineTableCellItem.identifier)
-//        collectionView.reloadData()
-//
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//
-//        if isHistory {
-//            return medicineBaskets.count
-//        } else {
-//            return medicineLibrary?.count ?? 1
-//        }
-//
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: contentCellId, for: indexPath) as? MedicineTableCellItem else {
-//            return UICollectionViewCell()
-//        }
-//
-//        if isHistory {
-//            if indexPath.row == medicineBaskets.count - 1 {
-//                cell.viewDivider.isHidden = true
-//            } else {
-//                cell.viewDivider.isHidden = false
-//            }
-//            cell.medicineBasket = medicineBaskets[indexPath.row]
-//
-//        } else {
-//            if indexPath.row == (medicineLibrary?.count ?? 1) - 1 {
-//                cell.viewDivider.isHidden = true
-//            } else {
-//                cell.viewDivider.isHidden = false
-//            }
-//            cell.medicineEntries = medicineEntries
-//            cell.medicineItem = medicineLibrary?[indexPath.row]
-//
-//        }
-//        cell.backgroundColor = .red
-//        return cell
-//    }
-//
-//}
-//
-
-
-
-
 // MARK: - Kuburan
-
-//    func setupFilter() {
-//        var filterViews = [filter1, filter2, filter3]
-//        var filterLabels = [filter1Label, filter2Label, filter3Label]
-//        var filterButtons = [filter1Button, filter2Button, filter3Button]
-//
-//        if filterStrings.count != 0 {
-//            filterUtama.borderWidth = 1
-//            filterUtama.backgroundColor = filter1.backgroundColor
-//            filterUtama.borderColor = .iconIolite
-//            filterUtamaLabel.textColor = .iconIolite
-//            filterUtamaLabel.text = "Yahud"
-//        } else {
-//            filterUtama.borderColor = .disabled
-//            filterUtamaLabel.textColor = .black
-//            filterUtamaLabel.text = "Yahud"
-//        }
-//
-//        for int in 0..<filterStrings.count {
-//            if int < 3 {
-//                filterLabels[int]?.text = filterStrings[int]
-//            }
-//            else {
-//                    filterLabels[3]?.text = "+\(categories.count-3)"
-//            }
-//        }
-//
-//        for fView in filterViews {
-//            fView?.alpha = 1
-//        }
-//
-//        for int in 0..<filterStrings.count {
-//            if int < 3 {
-//                filterViews.remove(at: 0)
-//            }
-//        }
-//
-//        for fView in filterViews {
-//            fView?.alpha = 0
-//        }
-//
-//        for fView in filterViews {
-//            view.addSubview(fView!)
-//        }
-//    }
