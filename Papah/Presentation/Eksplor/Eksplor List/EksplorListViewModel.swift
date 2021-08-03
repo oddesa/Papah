197c3b422502ec2a352cbce7cb2583229131ad43
Kane <jehnsen.hirena@sci.ui.ac.id>
2021-08-03 13:15:39 +0700

    add logic to check availability of WBKL

--- Papah/Presentation/Eksplor/Eksplor List/EksplorListViewModel.swift
+++ Papah/Presentation/Eksplor/Eksplor List/EksplorListViewModel.swift
@@ -24,7 +24,76 @@ class EksplorListViewModel: NSObject {
     func getWBklData() -> [Wbkl]? {
         return wbklRepository.getAllWbkl()
     }
-        
-}
+    
+    func extractNameOfTheDay() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "id_ID")
+        dateFormatter.dateFormat = "EEEE"
+        let dayInWeek = dateFormatter.string(from: date)
+        return dayInWeek
+    }
 
-//    let wbklData = WBKL(name: "nama", lng: 1, lat: 2, img: UIImage.whatsAppImage20210719At085013.jpegData(compressionQuality: 1.0) ?? Data(), operationalDay: "08:00", operationalHour: "08:00", address: "213", phoneNumber: "123")
+    func extractHours() -> Int {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "id_ID")
+        dateFormatter.dateFormat = "HH"
+        let hourInDay = dateFormatter.string(for: date)
+        return Int(hourInDay!)!
+    }
+
+    func extractMinutes() -> Int {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "id_ID")
+        dateFormatter.dateFormat = "mm"
+        let minutesInHour = dateFormatter.string(for: date)
+        return Int(minutesInHour!)!
+    }
+
+    func countOperationalDay(text: String) -> Int {
+        var splited = text.components(separatedBy: ["-", " "])
+        splited = splited.filter {$0 != ""}
+        return splited.count
+    }
+
+    func extractOperationalTime(operationalHour: String) -> [Int] {
+        var splited = operationalHour.components(separatedBy: ["-", " ", "."])
+        var operationalHourArray: [Int] = []
+        splited = splited.filter {$0 != ""}
+        for angka in splited {
+            let integiii = Int(angka)
+            operationalHourArray.append(integiii!)
+        }
+        return operationalHourArray
+    }
+
+    func timeChecker(operationalHour: String) -> Bool {
+        let hourNow = extractHours()
+        let minuteNow = extractMinutes()
+        let openTimeArr = extractOperationalTime(operationalHour: operationalHour)
+        if openTimeArr[0] <= hourNow && openTimeArr[1] <= minuteNow {
+            if openTimeArr[2] >= hourNow && openTimeArr[3] <= minuteNow {
+                return true
+            } else {return false}
+        } else {return false}
+    }
+
+
+    func bukaTutupChecker(operationalDay: String, operationalHour: String) -> Bool {
+        if countOperationalDay(text: operationalDay) == 1 {
+            let dayName = extractNameOfTheDay()
+            if operationalDay == dayName {
+                return timeChecker(operationalHour: operationalHour)
+            } else {return false}
+        } else {
+            //ini karena senin - minggu tok
+            let daysOfTheWeek = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"]
+            let dayName = extractNameOfTheDay()
+            if daysOfTheWeek.contains(dayName) {
+                return timeChecker(operationalHour: operationalHour)
+            } else {return false}
+        }
+    }
+}
