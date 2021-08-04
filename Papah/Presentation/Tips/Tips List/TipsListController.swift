4438a828bade6c3f2a48c607df489f21341d94db
Rizqi Ahmad Kurniawan <rizqikurniawan233@gmail.com>
2021-08-04 20:30:57 +0700

    Files changed (updated): TipsListStoryboard (auto-layout) SubtitleTableViewCell (auto-layout) MedaliDetail (auto-layout)

--- Papah/Presentation/Tips/Tips List/TipsListController.swift
+++ Papah/Presentation/Tips/Tips List/TipsListController.swift
@@ -20,9 +20,6 @@ class TipsListController: MVVMViewController<TipsListViewModel>, UITableViewDele
         print("CELL \(cell)")
         return cell
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
-    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("DID SELECT")
