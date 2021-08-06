3192b71efff694aa53130b0df2d1a03e5ee8cdc5
Delvina J <j.delvina@gmail.com>
2021-08-07 00:09:05 +0700

    update tantangan page, badge preload data, UI Minor fixing
    Tantangan page:
    - Update progress bar value
    - badge view (Medali + medali details)
    - badge logic
    - Core Data Badge Entity (add image_achieved)
    - Preload Badge (add image, change monthly badge id)

--- Papah/Core/CoreDataManager.swift
+++ Papah/Core/CoreDataManager.swift
@@ -110,9 +110,11 @@ extension CoreDataManager {
             
         if WbklDataRepository.shared.getAllWbkl().count == 0 {
             preloadDataWbkl()
+            preloadBadgeCategory()
             preloadWasteCategoryofWbkl()
             preloadDataTips()
             preloadDataTipsDetail()
+            preloadBadges()
             preloadWasteCategory()
             preloadMonthlyChallenges()
             preloadMonthlyChallengeProgress()
--- Papah/Presentation/Tantangan/Medali List/Cell/Collection Cell/MedaliListCollectionViewCell.swift
+++ Papah/Presentation/Tantangan/Medali List/Cell/Collection Cell/MedaliListCollectionViewCell.swift
@@ -12,6 +12,9 @@ class MedaliListCollectionViewCell: UICollectionViewCell {
     static let identifier = "MedaliListCollectionViewCell"
     
     @IBOutlet weak var medaliDetail: UIButton!
+    @IBOutlet weak var medalTitle: UILabel!
+    @IBOutlet weak var medalDesc: UILabel!
+    @IBOutlet weak var medalImg: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
