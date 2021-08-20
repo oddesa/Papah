6add0e31e0fabacca630598c87cb0c8659695bf1
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-08-20 22:56:31 +0700

    Initial commit

---
+++ Watch Extension/Reward/RewardView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Watch Extension
+//
+//  Created by Dhiky Aldwiansyah on 20/08/21.
+//
+
+import SwiftUI
+
+struct RewardView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct RewardView_Previews: PreviewProvider {
+    static var previews: some View {
+        RewardView()
+    }
+}
