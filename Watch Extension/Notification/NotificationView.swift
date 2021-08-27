8af89482e4b95ff9dea5e50511c8fe5e974c2c3d
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-08-27 09:01:36 +0700

    Implement frontend for notification WatchOS

--- Watch Extension/Notification/NotificationView.swift
+++ Watch Extension/Notification/NotificationView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct NotificationView: View {
+    var title: String?
+    var message: String?
+    
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+                VStack {
+            //            Image(systemName: "photo")
+            //            Text("Dapat Rewards")
+                    Image("33Watch").resizable().aspectRatio(contentMode: .fit)
+                        .frame(width: 110, height: 110, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Text(title ?? "Dapat Rewards")
+                        .font(.headline)
+                    Divider()
+                    Text(message ?? "Berhasil Meraih \"Kurcaci Penyalur\" ")
+                        .font(.caption)
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Lihat Rewards")
+                    }
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Tutup")
+                    }
+                }
+//                .lineLimit(0)
+                
+            }
+        
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        Group{
+            NotificationView()
+            NotificationView(title: "Dapat Rewards",
+                                         message: "Berhasil Meraih \"Kurcaci Penyalur\" "
+                            )
+
+        }
+        
+        
     }
 }
