3d991a83feb68b7440f5750c57eb2bf33e94dfc1
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-08-29 22:14:47 +0700

    Fix eksplor list data

--- Papah/Presentation/Eksplor/Eksplor List/EksplorListViewModel.swift
+++ Papah/Presentation/Eksplor/Eksplor List/EksplorListViewModel.swift
@@ -8,34 +8,187 @@
 import UIKit
 import CoreLocation
 import MapKit
-
-class WbklPro: WbklDataRepository {
-    var jarak: Double
-    let wbklData: Wbkl
-    var categories: [String]
-    
-    
-    init(jarak: Double, wbkl: Wbkl, categories: [String] ) {
-        self.jarak = jarak
-        self.wbklData = wbkl
-        self.categories = categories
-    }
-}
+import Combine
 
 class EksplorListViewModel: NSObject {
-
+    
     let wbklRepository = WbklDataRepository.shared
-    var isItEmptyState = false
     var allWbkl = [WbklPro]()
+    var filteredWbkl = [WbklPro]()
+    var filterCategories: [WasteCategory] = []
     
-    func getWBklData() -> [Wbkl]? {
-        return wbklRepository.getAllWbkl()
+    var isItEmptyState = CurrentValueSubject<Bool,Never>(false)
+    
+    // MARK: - Distance Logic
+    let locationManager = CLLocationManager()
+    var userLocation: [CLLocation]?
+    
+    func getWbklBasedOnSearch(text: String,
+                              filterCategories: [WasteCategory]) {
+        
+        let splited = text.components(separatedBy: " ")
+        
+        if text.count != 0 {
+                        
+            filteredWbkl = allWbkl.filter {
+                if let name = $0.wbklData.name {
+                    for word in splited {
+                        let nameCheck = name.lowercased().contains(word.lowercased())
+                        let categoryCheck = !$0.categories.filter { $0.lowercased().contains(word.lowercased()) }.isEmpty
+                        if nameCheck || categoryCheck {
+                            if let index = $0.categories.firstIndex(of: word.capitalized) {
+                                $0.categories = rearrangeArray(array: $0.categories, fromIndex: index, toIndex: 0)
+                            }
+                            return true
+                        }
+                    }
+                }
+                return false
+            }
+            
+            self.wbklCategoryFiltered(withSearch: true)
+            
+//            for wbkl in allWbkl {
+//
+//                let nameCheck = (wbkl.wbklData.name ?? "").lowercased().contains(text.lowercased())
+//                let categoryCheckText = (categoriesChecker(wbkl: wbkl, word: text))
+//
+//                if nameCheck && categoryCheckText {
+//
+//                    if let index = wbkl.categories.firstIndex(of: text.capitalized) {
+//                        wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
+//                    }
+//
+//                    filteredWbkl.append(wbkl)
+//                } else {
+//                    for word in splited {
+//
+//                        let categoryCheckWord = (categoriesChecker(wbkl: wbkl, word: word))
+//
+//                        if nameCheck && categoryCheckWord {
+//                            if let index = wbkl.categories.firstIndex(of: word.capitalized) {
+//                                wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
+//                            }
+//                            filteredWbkl.append(wbkl)
+//                        }
+//                    }
+//                }
+//            }
+            
+        }
+        
+//        for wbkl in allWbkl {
+//            var textName = text
+//
+//            for word in splited {
+//                if (categoriesChecker(wbkl: wbkl, word: word)) {
+//                    textName = textName.lowercased().replacingOccurrences(of: " \(word.lowercased())", with: "")
+//                    textName = textName.lowercased().replacingOccurrences(of: "\(word.lowercased()) ", with: "")
+//                    if let index = wbkl.categories.firstIndex(of: word.capitalized) {
+//                        wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
+//                    }
+//                }
+//            }
+//            if (wbkl.wbklData.name?.lowercased().contains(textName.lowercased())) ?? false  {
+//                let idx = allWbkl.firstIndex(where: { $0 === wbkl })
+//                filteredWbkl = (rearrangeArray(array: allWbkl, fromIndex: idx ?? 0, toIndex: 0))
+//            }
+//        }
+        
+        print("FILTERED WBKL \(filteredWbkl.count)")
+
+        if self.filteredWbkl.count == 0 {
+            self.isItEmptyState.send(true)
+        } else {
+            self.isItEmptyState.send(false)
+        }
+        
     }
     
-    func getAllWasteCategory() -> [WasteCategory] {
-        return wbklRepository.getAllWasteCategory()
+    //MARK: KEN ASINKRONUS
+    // MARK: - Update Wbkl to WbklPro
+    func setupWbklProData(){
+        
+        if allWbkl.isEmpty {
+            for wbkl in wbklRepository.getAllWbkl() {
+                let wbklPro = WbklPro(jarak: 0, wbkl: wbkl, categories: self.getWbklCategoriesName(wbkl: wbkl))
+                allWbkl.append(wbklPro)
+            }
+            filteredWbkl = allWbkl
+        } else {
+            filteredWbkl = allWbkl
+        }
+        
+        if self.filteredWbkl.count == 0 {
+            self.isItEmptyState.send(true)
+        } else {
+            self.isItEmptyState.send(false)
+        }
+        
     }
     
+    func updateWbklDistance(completion: ( () -> Void)? = nil){
+
+        var locationCount = 0
+
+        if let userLocation = userLocation?.last {
+            for (index, data) in self.allWbkl.enumerated() {
+                getLocationDistance(wbklPro: data, userLocation:  userLocation) { jaraDbl in
+                    
+                    self.allWbkl[index].jarak = jaraDbl
+                    locationCount += 1
+                    
+                    if locationCount == self.allWbkl.count {
+                        let sortedWbklsJarak = self.sortBasedOnDistance(wbklPros: self.allWbkl)
+                        self.allWbkl = sortedWbklsJarak
+                        self.filteredWbkl = sortedWbklsJarak
+                        completion?()
+                    }
+                }
+            }
+            
+        }
+        else {
+            for (index, _) in self.allWbkl.enumerated() {
+                self.allWbkl[index].jarak = 0
+            }
+            let sortedWbklsJarak = self.sortBasedOnDistance(wbklPros: self.allWbkl)
+            self.allWbkl = sortedWbklsJarak
+            self.filteredWbkl = sortedWbklsJarak
+            completion?()
+        }
+    }
+    
+    func wbklCategoryFiltered(withSearch: Bool) {
+        self.filteredWbkl = self.filterBasedOnCat(withSearch: withSearch)
+        
+        if self.filteredWbkl.count == 0 {
+            self.isItEmptyState.send(true)
+        } else {
+            self.isItEmptyState.send(false)
+        }
+        
+        print("FILTERED WBKL \(filteredWbkl.count)")
+    }
+    
+}
+
+extension EksplorListViewModel {
+    
+    func rearrangeArray<T>(array: Array<T>, fromIndex: Int, toIndex: Int) -> Array<T> {
+        var arr = array
+        let element = arr.remove(at: fromIndex)
+        arr.insert(element, at: toIndex)
+        return arr
+    }
+    
+    // MARK: - Search Logic
+    func categoriesChecker(wbkl: WbklPro, word: String) -> Bool {
+        let categoryData = wbkl.categories.filter { $0.lowercased().contains(word.lowercased()) }
+        return !categoryData.isEmpty
+    }
+    
+    // MARK: - Get WBKL Categories name
     func getWbklCategoriesName (wbkl: Wbkl) -> [String] {
         guard let categoriesData = wbkl.wasteAccepted?.allObjects as? [WasteAccepted] else {return [" "]}
         var categoriesString: [String] = []
@@ -45,104 +198,6 @@ class EksplorListViewModel: NSObject {
         return categoriesString
     }
     
-    
-    
-    // MARK: - Search Logic
-    
-    func categoriesChecker(wbkl: WbklPro, word: String) -> Bool {
-        for category in wbkl.categories {
-            if category.lowercased().contains(word.lowercased()) {
-                return true
-            }
-        }
-        return false
-    }
-    
-    func getWbklBasedOnSearch(text: String, dataWbkl: [WbklPro],
-                              filterCategories: [WasteCategory]) {
-        
-        let splited = text.components(separatedBy: " ")
-    
-        if text.count == 0 {
-            if filterCategories.count == 0 {
-                allWbkl = []
-            } else {
-                allWbkl = dataWbkl
-            }
-        } else {
-            allWbkl = []
-            for wbkl in dataWbkl {
-                if (wbkl.wbklData.name ?? "").lowercased().contains(text.lowercased()) || (categoriesChecker(wbkl: wbkl, word: text)) {
-                    print(text.lowercased())
-                    
-                    if (categoriesChecker(wbkl: wbkl, word: text)) {
-                        if let index = wbkl.categories.firstIndex(of: text.capitalized) {
-                            wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
-                        }
-                    }
-                    
-                    allWbkl.append(wbkl)
-                } else {
-                    for word in splited {
-                        if (wbkl.wbklData.name ?? "").lowercased().contains(word.lowercased()) || (categoriesChecker(wbkl: wbkl, word: word)) {
-                            print(word.lowercased())
-                            
-                            if (categoriesChecker(wbkl: wbkl, word: word)) {
-                                if let index = wbkl.categories.firstIndex(of: word.capitalized) {
-                                    wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
-                                }
-                            }
-                            allWbkl.append(wbkl)
-                        }
-                    }
-                }
-            }
-        }
-        
-        allWbkl = (removeDuplicatesWbkl(wbklPros: allWbkl))
-        
-        for wbkl in allWbkl {
-            var textName = text
-            
-            for word in splited {
-                if (categoriesChecker(wbkl: wbkl, word: word)) {
-                    textName = textName.lowercased().replacingOccurrences(of: " \(word.lowercased())", with: "")
-                    textName = textName.lowercased().replacingOccurrences(of: "\(word.lowercased()) ", with: "")
-                    if let index = wbkl.categories.firstIndex(of: word.capitalized) {
-                        wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
-                    }
-                }
-            }
-            if (wbkl.wbklData.name?.lowercased().contains(textName.lowercased())) ?? false  {
-                let idx = allWbkl.firstIndex(where: { $0 === wbkl })
-                allWbkl = (rearrangeArray(array: allWbkl, fromIndex: idx ?? 0, toIndex: 0))
-            }
-        }
-        allWbkl = filterBasedOnCat(allWbkl: allWbkl, filterCategories: filterCategories)
-
-    }
-    
-    // MARK: - Duplicate Remover Logic (non hashable)
-    func removeDuplicatesWbkl(wbklPros: [WbklPro]) -> [WbklPro] {
-        var uniques = [WbklPro]()
-        for wbkl in wbklPros {
-            if !uniques.contains(where: {$0.wbklData.id == wbkl.wbklData.id}) {
-                uniques.append(wbkl)
-            }
-        }
-        return uniques
-    }
-    
-    func removeDuplicatesCategories(categories: [WasteCategory]) -> [WasteCategory] {
-        var uniques = [WasteCategory]()
-        for cat in categories {
-            if !uniques.contains(where: {$0.title == cat.title }) {
-                uniques.append(cat)
-            }
-        }
-        return uniques
-    }
-    
     // MARK: - Sorter Filter Logic
     func sortBasedOnDistance(wbklPros: [WbklPro]) -> [WbklPro] {
         let sortedWbklJarak = wbklPros.sorted {
@@ -150,129 +205,24 @@ class EksplorListViewModel: NSObject {
         }
         return sortedWbklJarak
     }
-
     
-    func rearrangeArray<T>(array: Array<T>, fromIndex: Int, toIndex: Int) -> Array<T> {
-        var arr = array
-        let element = arr.remove(at: fromIndex)
-        arr.insert(element, at: toIndex)
-        return arr
-    }
-
-    func filterBasedOnCat(allWbkl: [WbklPro], filterCategories: [WasteCategory]) -> [WbklPro]{
-        var dataWbkl = allWbkl
-        for wbkl in dataWbkl {
-            for category in filterCategories {
-                if !(wbkl.categories.contains(category.title ?? " ")) {
-                    if let idx = dataWbkl.firstIndex(where: { $0 === wbkl }) {
-                        dataWbkl.remove(at: idx)
-                    }
-                    
-                }
-                
-                let word = category.title ?? ""
-                if (categoriesChecker(wbkl: wbkl, word: word)) {
-                    if let index = wbkl.categories.firstIndex(of: word.capitalized) {
-                        wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
-                    }
-                }
-                
-            }
-            
+    // Filter category
+    func filterBasedOnCat(withSearch: Bool) -> [WbklPro]{
+        
+        let wbklData: [WbklPro] = withSearch ? self.filteredWbkl : self.allWbkl
+        
+        let filteredWbkl =  wbklData.filter { (wbkl) -> Bool in
+            let wbklCategory = wbkl.categories.map({ $0 })
+            let filteredCategories = self.filterCategories.map({$0.title ?? ""})
+            return filteredCategories.allSatisfy(wbklCategory.contains)
         }
-//        for category in filterCategories {
-//            let word = category.title ?? ""
-//            if (categoriesChecker(wbkl: wbkl, word: word)) {
-//                if let index = wbkl.categories.firstIndex(of: word.capitalized) {
-//                    wbkl.categories = rearrangeArray(array: wbkl.categories, fromIndex: index, toIndex: 0)
-//                }
-//            }
-//        }
-        
-        return dataWbkl
+        return filteredWbkl
     }
     
-
-    //MARK: KEN ASINKRONUS
-    // MARK: - Update Wbkl to WbklPro
-    func turnWbklsPro(completion: @escaping ([WbklPro]) -> Void){
-        guard let wbkls = getWBklData() else{fatalError("datanya ga keload")}
-        var wbklsPro: [WbklPro] = []
-        var locationCount = 0
-//        let wbklDispatchGroup = DispatchGroup()
-
-        for wbkl in wbkls {
-//            wbklDispatchGroup.enter()
-            getLocationDistance(wbklData: wbkl, userLocation:  (userLocation?.last ?? locationDummy)) { jarakStr,jaraDbl in
-                let wbklPro = WbklPro(jarak: jaraDbl, wbkl: wbkl, categories: self.getWbklCategoriesName(wbkl: wbkl))
-                wbklsPro.append(wbklPro)
-                if wbklPro.categories.count == 0 {
-                    fatalError("cuagas")
-                }
-                locationCount += 1
-                print("current countt \(locationCount) && \(wbkls.count)")
-                print("DISTANCE \(jaraDbl) srt \(jarakStr)")
-
-                if locationCount == wbkls.count {
-                    print("TEST LEAVE")
-                    print("TEST WBKPRO \(wbklsPro)")
-//                    self.wbklDispatchGroup.leave()
-                    let sortedWbklsJarak = self.sortBasedOnDistance(wbklPros: wbklsPro)
-                    completion(sortedWbklsJarak)
-                }
-            }
-           
-        }
-        
-//        wbklDispatchGroup.notify(queue: DispatchQueue.main) {
-//            print("Finished all requests.")
-//            print("TEST WBKPRO \(wbklsPro)")
-//        }
-
-    }
-
-    //MARK: KEN ASINKRONUS
-    func returnWbklsBasedOnCat(filterCategories: [WasteCategory], completion: ( () -> Void)? = nil ) {
-        turnWbklsPro(completion: { data in
-            self.allWbkl = data
-            self.allWbkl = self.filterBasedOnCat(allWbkl: self.allWbkl, filterCategories: filterCategories)
-            completion?()
-        })
-        
-    }
-    
-    // MARK: - Distance Logic
-    let locationManager = CLLocationManager()
-    let locationDummy = CLLocation(latitude: -6.636076, longitude: 106.804472)
-    var userLocation: [CLLocation]?
-    
-//    func getLocationDistance(userLocation: CLLocation, wbklData: Wbkl) -> Double {
-//
-//        let targetLocation = CLLocation(latitude: Double(wbklData.latitude), longitude: Double(wbklData.longitude))
-//        let userLocation = CLLocation(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
-//
-//        return targetLocation.distance(from: userLocation)
-//    }
-//
-//    func locationDistanceString(distanceInMeter: Double) -> String {
-//        var distanceInMeter = distanceInMeter
-//        var distanceInString: String
-//        if distanceInMeter >= 1000 {
-//            let distanceInKM = distanceInMeter / 1000
-//            let distanceInKMRounded = distanceInKM.rounded()
-//            distanceInString = "\(distanceInKMRounded) km"
-//        }
-//        else {
-//            distanceInMeter = distanceInMeter.rounded()
-//            distanceInString = "\(distanceInMeter) m"
-//        }
-//        return distanceInString
-//    }
     
     //MARK: KEN ASINKRONUS
-    func getLocationDistance(wbklData: Wbkl?, userLocation: CLLocation, completion: @escaping (String, Double) -> Void ) {
-        print("LAST LOC \(userLocation)")
-        if let wbklData = wbklData {
+    func getLocationDistance(wbklPro: WbklPro?, userLocation: CLLocation, completion: @escaping (Double) -> Void ) {
+        if let wbklData = wbklPro?.wbklData {
             
             let targetLocation = CLLocationCoordinate2D(latitude:Double(wbklData.latitude), longitude: Double(wbklData.longitude))
             let userLocation = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
@@ -293,29 +243,19 @@ class EksplorListViewModel: NSObject {
             direction.calculate { (response, error) in
                 guard let response = response else {
                     if let error = error {
-                        completion("", 0.0)
+                        completion(0.0)
                         print("No Route Found : \(error.localizedDescription)")
                     }
                     return
                 }
                 
+                print("Route Check \(response.routes)")
+
                 if response.routes.count < 1 {
-                    print("RESPONSE RTOUREE eee")
-                    completion("", 0.0)
+                    completion(0.0)
                 } else {
-                    
-                    var distanceInMeter = response.routes[0].distance
-                    var distanceInString: String
-                    if distanceInMeter >= 1000 {
-                        let distanceInKM = distanceInMeter / 1000
-                        let distanceInKMRounded = distanceInKM.rounded()
-                        distanceInString = "\(String.init(format: "%.0f", distanceInKMRounded))km"
-                    }
-                    else {
-                        distanceInMeter = distanceInMeter.rounded()
-                        distanceInString = "\(String.init(format: "%.3f", distanceInMeter))m".replacingOccurrences(of: "0.", with: "")
-                    }
-                    completion(distanceInString, distanceInMeter)
+                    let distanceInMeter = response.routes[0].distance
+                    completion(distanceInMeter)
                 }
             }
         }
---
+++ Papah/Presentation/Eksplor/Eksplor List/Model/WbklPro.swift
@@ -0,0 +1,37 @@
+//
+//  WbklPro.swift
+//  Papah
+//
+//  Created by Dhiky Aldwiansyah on 28/08/21.
+//
+
+import Foundation
+
+class WbklPro {
+    var jarak: Double
+    let wbklData: Wbkl
+    var categories: [String]
+    
+    init(jarak: Double, wbkl: Wbkl, categories: [String] ) {
+        self.jarak = jarak
+        self.wbklData = wbkl
+        self.categories = categories
+    }
+    
+    func getJarakInString() -> String {
+        if jarak >= 1000 {
+            let distanceInKM = jarak / 1000
+            let distanceInKMRounded = distanceInKM.rounded()
+            return " · \(String.init(format: "%.0f", distanceInKMRounded))km"
+        }
+        else {
+            jarak = jarak.rounded()
+            
+            if jarak == 0 {
+                return ""
+            } else {
+                return " · \(String.init(format: "%.3f", jarak))m".replacingOccurrences(of: "0.", with: "")
+            }
+        }
+    }
+}
