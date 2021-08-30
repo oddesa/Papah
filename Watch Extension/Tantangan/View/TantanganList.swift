6319128f59fb5f70edd99e8312e0e8c16e36beae
Kane <jehnsen.hirena@sci.ui.ac.id>
2021-08-31 02:53:13 +0700

    dah

--- Watch Extension/Tantangan/View/TantanganList.swift
+++ Watch Extension/Tantangan/View/TantanganList.swift
@@ -15,26 +15,13 @@ struct TantanganList: View {
     
     var body: some View {
         
-//        let maxValueFloat = Float(monthChallProgress.monthlyChallenge?.max_value ?? 0)
-//        let currentValue = String(format: "%.0f", monthChallProgress.current_value)
         let maxValueStr = String(format: "%.0f", monthChallProgress.monthlyChallenge!.max_value)
         let progressToGo = monthChallProgress.monthlyChallenge!.max_value - monthChallProgress.current_value
         let percent = monthChallProgress.current_value / monthChallProgress.monthlyChallenge!.max_value
         let progressToGoStr = String(format: "%.0f", progressToGo)
         let percentStr = String(format: "%.0f", percent)
         let maxValueCount = String(maxValueStr).count
-//        mcTextProgress.text = "\(currentValue) / \(maxValue)"
-//        mcProgressBar.progress = mcp.current_value / maxValueFloat
-//
-//        if mcp.status == true {
-//            mcClaimPointDesc.text = "Kamu telah klaim 300 poin"
-//            mcClaimPointDesc.textColor = .systemGreen
-//            mcClaimPointIcon.image = UIImage._41_img
-//        } else {
-//            mcClaimPointDesc.text = "+300 poin"
-//            mcClaimPointDesc.textColor = .gray
-//            mcClaimPointIcon.image = UIImage._40_img
-//        }
+
         
         HStack(spacing: 10.0) {
             Image(image)
